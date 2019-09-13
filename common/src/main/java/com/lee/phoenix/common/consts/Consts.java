cd1a8d92cf1ae754b9721997d803a8cb8d93a897
lilee <997977130@qq.com>
2019-09-13 18:42:51 +0800

    fire to the sky

--- common/src/main/java/com/lee/phoenix/common/consts/Consts.java
+++
@@ -1,9 +0,0 @@
-package com.lee.phoenix.common.consts;
-
-import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.List;
-
-public class Consts {
-
-}
--- web/src/main/java/com/lee/phoenix/contract/auth/TUser.java
+++
@@ -1,4 +0,0 @@
-package com.lee.phoenix.contract.auth;
-
-public class TUser {
-}
